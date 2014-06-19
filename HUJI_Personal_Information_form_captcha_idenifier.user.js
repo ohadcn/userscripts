a7ab860e6cbda2f2b0cdfe421c6e457cd9b34240
Ohad Cohen <ohadcn@gmail.com>
2014-06-19 12:15:34 +0300

    bug fix :working on page https://www.huji.ac.il/dataj/controller/auth/stu

--- HUJI_Personal_Information_form_captcha_idenifier.user.js
+++ HUJI_Personal_Information_form_captcha_idenifier.user.js
@@ -2,10 +2,11 @@
 // @id             HUJIINFOOCR
 // @name           HUJI Personal Information form captcha idenifier
 // @description    Auto fills the form to get login the personal information
-// @version        1.1
+// @version        1.1.2
 // @namespace      ohadcn
 // @author         Ohad Cohen & Shlomi Shasha
-// @include        https://www.huji.ac.il/dataj/controller/stu/?
+// @include        https://www.huji.ac.il/dataj/controller/stu*
+// @include        https://www.huji.ac.il/dataj/controller/auth/stu*
 // @require        https://code.jquery.com/jquery-2.0.3.min.js
 // @grant          GM_xmlhttpRequest
 // @updateURL      http://www.cs.huji.ac.il/~omershe/HUJI_Personal_Information_form_captcha_idenifier.meta.js
