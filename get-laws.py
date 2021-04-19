b482fac642d8aa3f29c36986a1842995c744ddbb
Ohad Cohen <ohadcn@gmail.com>
2021-04-19 12:49:16 +0300

    minor changes

--- get-laws.py
+++ get-laws.py
@@ -5,7 +5,10 @@ from os import path
 from json import load, dump
 from re import compile as regex
 from docx import Document as docx
-from requests.exceptions import RequestException 
+try:
+	from requests.exceptions import RequestException
+except:
+	from requests import RequestException
 # ['DEFAULT_ORDER_BY_FIELD', 'DEFAULT_REQUEST_TIMEOUT_SECONDS', 'METHOD_NAME', 'ORDERED_FIELDS', 'SERVICE_NAME', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_entry', '_fields', '_get_all_pages', '_get_instance_from_entry', '_get_method_name', '_get_request_exception', '_get_response_content', '_get_service_name', '_get_soup', '_get_url_base', '_get_url_page', '_get_url_single', '_handle_prop', '_parse_entry', '_parse_entry_data', '_parse_entry_id', '_parse_entry_links', '_proxies', '_session', '_set_field_value', 'all_field_values', 'all_schema_field_values', 'committee_id', 'error_report', 'get', 'get_all', 'get_field', 'get_fields', 'get_json_table_schema', 'get_page', 'id', 'is_continuation', 'kns_num', 'last_update', 'magazine_num', 'name', 'num', 'page_num', 'postponent_reason_desc', 'postponent_reason_id', 'private_num', 'public_date', 'public_series_desc', 'public_series_first_call', 'public_series_id', 'status_id', 'sum_law', 'type_description', 'type_id']
 
 # OrderedDict([('id', 17791), ('kns_num', 16), ('name', 'הצעת חוק הגנת הפרטיות (תיקון - איסור פרסום בדבר עבר מיני של נפגע עבירת מין או אלימות), התשס"ד-2003'), ('type_id', 54), ('type_description', 'פרטית'), ('private_num', 1568), ('committee_id', None), ('status_id', 104), ('num', None), ('postponent_reason_id', None), ('postponent_reason_desc', None), ('public_date', None), ('magazine_num', None), ('page_num', None), ('is_continuation', None), ('sum_law', None), ('public_series_id', None), ('public_series_desc', None), ('public_series_first_call', None), ('last_update', datetime.datetime(2013, 5, 23, 18, 37, 57))])
@@ -76,13 +79,14 @@ for bill in bills:
 				status.append(0)
 
 			if(len(p23) < bill["private_num"]+1):
-				p23.extend((bill["private_num"]+1-len(p23))*[[0, 0, 0, 0, 0]])
+				p23.extend((bill["private_num"]+1-len(p23))*[[0, 0, 0, 0, 0, 0]])
 			status.append(bill["name"])
 			status.append("https://main.knesset.gov.il/Activity/Legislation/Laws/Pages/LawBill.aspx?t=lawsuggestionssearch&lawitemid=" + str(bill["id"]))
 			status.append(bill["postponent_reason_desc"])
+			status.append(bill["postponent_reason_id"])
 			p23[bill["private_num"]] = status
 			
-open(path.join(mydir, "file.csv"), "wt").write("\n".join(["{},{},\"{}\",{},{}".format(*law) for law in p23]))
+open(path.join(mydir, "file.csv"), "wt").write("\n".join(["{},{},\"{}\",{},{},{}".format(*law) for law in p23]))
 if(storeme != None):
 	dump([{key:o[key] for key in o.keys() if type(o[key]) != datetime} for o in storeme], open(datafilename, "wt"))
 print(datetime.now() - start)
